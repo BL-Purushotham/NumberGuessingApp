fafb23590be85901838b2cab78c8b33521d43295
BL-Purushotham <purushotham.cirimala@bridgelabz.com>
2026-02-06 14:57:56 +0530

    feature(UC1): Game Initialization

---
+++ src/main/java/com/bridgelabz/guessnumber/GameConfig.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.guessnumber;
+
+import java.util.Random;
+
+/**
+ * Use Case 1: Game Initialization
+ *
+ * This class is responsible for:
+ * - Setting game boundaries
+ * - Generating a random target number
+ * - Displaying game rules
+ *
+ * Demonstrates:
+ * - Encapsulation
+ * - Constructor initialization
+ * - Random number generation
+ */
+
+public class GameConfig {
+
+    private final int MIN = 1;
+    private final int MAX = 100;
+    private final int MAX_ATTEMPTS = 7;
+    private final int MAX_HINTS = 3;
+
+    int targetNumber;
+
+    public GameConfig(){
+        Random random = new Random();
+        this.targetNumber = random.nextInt(MAX-MIN+1) + MIN;
+    }
+
+    public int getTargetNumber(){
+        return targetNumber;
+    }
+
+    public int getMAX_HINTS(){
+        return MAX_HINTS;
+    }
+
+    public int getMAX_ATTEMPTS(){
+        return MAX_ATTEMPTS;
+    }
+
+    public void showRules(){
+        System.out.println("Guess a number between " + MIN + " and " + MAX);
+        System.out.println("You have " + MAX_ATTEMPTS + " attempts.");
+        System.out.println("Hints will be provided after wrong guesses.\n");
+    }
+}
